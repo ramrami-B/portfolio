12863b47d92f72e281e4db8654fde542579ddc20
ramrami-B <hrb.harper@gmail.com>
2024-05-30 14:35:15 +0900

    🐛: ReferenceError: module is not defined 해결

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+export default {
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
