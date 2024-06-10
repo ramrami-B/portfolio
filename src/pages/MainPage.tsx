4159687909cc6b8eaca54755ac882a9668510458
ramrami-B <hrb.harper@gmail.com>
2024-06-10 16:41:22 +0900

    🔧: tailwind 폰트 설정 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,9 @@
 const MainPage = () => {
-  return <div className="text-xl font-bold">Main</div>;
+  return (
+    <div className="w-screen h-screen bg-black">
+      <h1 className="text-white font-nm-reg">Haram Baek's Portfolio</h1>
+    </div>
+  );
 };
 
 export default MainPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "nm-reg": "NanumMyeongjo",
+      "nm-bold": "NanumMyeongjoBold",
+      "nm-extra-bold": "NanumMyeongjoExtraBold",
+      "roboto": "roboto",
+    },
   },
   plugins: [],
 };
